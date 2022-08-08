22024dbc7aede56e04c97cb7b56f34b09d1d25d6
zurimedina <medina.zuri@gmail.com>
2022-08-08 20:30:20 +0100

    State en compoentes

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
 import Mensaje from './Mensaje'
+import { useState } from 'react'
 
-const App = () => {
+//component
+const Counter = ({ contador }) => {
+  return <h1>{contador}</h1>
+}
+
+const App = (props) => {
+
+  /* const contador = useState(0);
+const contadorValue = contador[0];
+const updateContador = contador[1]; */
+
+  //Hook useState
+  const [contadorValue, updateContador] = useState(0)
+
+  const handleClick = () => {
+    updateContador(contadorValue + 1)
+  }
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+
   const firstName = 'Zuri'
 
   return (
     <div className="App">
-      <Mensaje color='red' message='Estamos trabajando, tal'/>
+
+      <button onClick={handleClick}>
+        incrementar
+      </button>
+
+      <Counter contador={contadorValue} />
+
+      <p>{left}</p>
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+
+      <p>{right}</p>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+
+      <Mensaje color='red' message='Estamos trabajando, tal' />
       {firstName}
     </div>
   );
